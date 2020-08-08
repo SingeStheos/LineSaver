24d52012a2ee352c42fb980032ffe7478744c34a
SingeStheos <68999685+SingeStheos@users.noreply.github.com>
2020-08-08 07:28:05 -0700

    Add files via upload

---
+++ LineSaver/LineSaver.py
@@ -0,0 +1,20 @@
+#Hey! Listen! ⌘
+#This program was shared by Singe Stheos but was made and is owned by the lovely Secret#3621.
+#CREDIT TO ORIGINAL AUTHOR, THEY ARE COOL AND YOU SHOULD TALK TO THEM!
+
+print("THIS PROGRAM WILL EXPORT EVERY LINE IN INPUT.TXT TO AN INDIVIDUAL FILE. USE WITH CAUTION")
+print("type 'i understand' and hit enter to proceed.")
+if input() != "i understand":
+    exit()
+with open("input.txt", "r") as input:
+    count = 0
+    for line in input:
+        if not line:
+            print("Empty or last line, Exiting.")
+            break
+        with open(((4 - len("%X" % count)) * "0") + ("%X" % count) + ".txt", "w") as out:
+            out.write(line)
+            out.close()
+        count += 1
+print("Finished.")
+input.close()
---
+++ LineSaver/README.txt
@@ -0,0 +1,5 @@
+This piece of Python script was made in its entirety by Secret#3621 and edited for QOL and with addition of a credit to aforementioned programmer by Singe Stheos#5551.
+
+Please message the original programmer and thank him for his 500 byte creation.
+
+YOU MAY DISTRIBUTE IF YOU KEEP BOTH CREDIT AND README IN THEIR ORIGINAL STATES AND BUNDLED TOGETHER IN A .ZIP FILE
